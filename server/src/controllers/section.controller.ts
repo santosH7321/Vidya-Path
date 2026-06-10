368356a9213d001b33daa84dd62fe034a6da1a8c
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-06-10 09:58:42 +0530

    add updateSection and deleteSection controllers

--- server/src/controllers/section.controller.ts
+++ server/src/controllers/section.controller.ts
@@ -41,3 +41,56 @@ export const createSection = async (req: Request, res: Response) => {
         }
     }
 }
+
+export const updateSection = async (req: Request, res: Response) => {
+    try {
+        const {sectionName, sectionId} = req.body;
+
+        if(!sectionName || !sectionId) {
+            return res.status(400).json({
+                success:false,
+                message:'Missing Properties',
+            });
+        }
+
+        const section = await Section.findByIdAndUpdate(sectionId, {sectionName}, {new:true});
+
+        return res.status(200).json({
+            success:true,
+            message:'Section Updated Successfully',
+        });
+
+    }
+    catch(error) {
+        if(error instanceof Error){
+            return res.status(500).json({
+                success:false,
+                message:"Unable to update Section, please try again",
+                error:error.message,
+            });
+        }
+    }
+};
+
+export const deleteSection = async (req: Request, res: Response) => {
+    try {
+        const {sectionId} = req.params
+
+        await Section.findByIdAndDelete(sectionId);
+
+        return res.status(200).json({
+            success:true,
+            message:"Section Deleted Successfully",
+        })
+
+    }
+    catch(error) {
+        if(error instanceof Error){
+            return res.status(500).json({
+                success:false,
+                message:"Unable to delete Section, please try again",
+                error:error.message,
+            });
+        }
+    }
+}
