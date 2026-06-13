9787e7e163decaac6ecf68bfcd10071994170f61
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-06-13 10:36:02 +0530

    add categoryPageDetails controller

--- server/src/controllers/category.controller.ts
+++ server/src/controllers/category.controller.ts
@@ -25,3 +25,61 @@ export const createCategory = async (req: Request, res: Response) => {
 		});
 	}
 };
+
+export const showAllCategories = async (req: Request, res: Response) => {
+	try {
+		const allCategorys = await Category.find(
+			{},
+			{ name: true, description: true }
+		);
+		res.status(200).json({
+			success: true,
+			data: allCategorys,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			message: error instanceof Error ? error.message : "Error in showAllCategories",
+		});
+	}
+};
+
+export const categoryPageDetails = async (req: Request, res: Response) => {
+    try {
+            //get categoryId
+            const {categoryId} = req.body;
+            //get courses for specified categoryId
+            const selectedCategory = await Category.findById(categoryId)
+                                            .populate("courses")
+                                            .exec();
+            //validation
+            if(!selectedCategory) {
+                return res.status(404).json({
+                    success:false,
+                    message:'Data Not Found',
+                });
+            }
+            //get coursesfor different categories
+            const differentCategories = await Category.find({
+                                         _id: {$ne: categoryId},
+                                         })
+                                         .populate("courses")
+                                         .exec();
+
+            //return response
+            return res.status(200).json({
+                success:true,
+                data: {
+                    selectedCategory,
+                    differentCategories,
+                },
+            });
+
+    }
+    catch(error ) {
+        return res.status(500).json({
+            success:false,
+            message: error instanceof Error ? error.message : "Error in categoryPageDetails",
+        });
+    }
+}
